6ed03c14ee6e5239aaac3d506dc35ec325e25084
Do Tuan Huy <dotuanhuy1302@gmail.com>
2022-06-30 17:16:29 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// let cursor = document.getElementsByClassName("cursor");
+// let cursorFollow = document.getElementsByClassName("cursorFollow");
+
+// document.windows.on("mousemove", function () {
+//   console.log("mousemove");
+//   //   gsap.to(selector);
+// });
